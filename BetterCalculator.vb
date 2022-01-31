739e705633661712cfb480633f74f11b948962f2
Morgan Puckett <morganpuckett02@gmail.com>
2022-01-31 08:23:49 -0700

    FINAL

--- BetterCalculator.vb
+++ BetterCalculator.vb
@@ -14,15 +14,24 @@ Module BetterCalculator
         Dim goA As Boolean
         Dim goB As Boolean
         Dim goC As Boolean
-        Dim quitQue As Boolean
+        Dim goHome As Boolean
+        goA = False
+        goB = False
+        goC = False
+        goHome = False
         Dim menu As String
         Dim result As Integer
         menu = $"1. Add{vbNewLine}2. Subtract{vbNewLine}3. Muiltiply{vbNewLine}4. Divide"
-        quitQue = False
 
-        Do While quitQue = False
+
+        Do While goHome = False
+            goA = False
+            goB = True
+            goC = True
+            goHome = True
             Console.WriteLine("Please Enter 2 Numbers. Enter " & Chr(34) & "Q" & Chr(34) & " at anytime to quit.")
-            Do
+            Do While goA = False
+
                 Console.WriteLine("Choose a number:")
                 userResponseA = Console.ReadLine()
                 goA = True
@@ -30,85 +39,117 @@ Module BetterCalculator
                 Try
                     firstNumber = CInt(userResponseA)
                     goA = True
+                    goB = False
+                    goC = True
+                    goHome = True
+                    Console.WriteLine($"You entered ""{firstNumber}""")
                 Catch ex As Exception
 
                     Select Case userResponseA
 
                         Case "q"
-                            quitQue = True
                             goA = True
+                            goB = True
+                            goC = True
+                            goHome = True
                         Case Else
                             Console.WriteLine($"You entered ""{userResponseA}"", please enter a whole number.")
 
                             goA = False
                     End Select
 
+
                 End Try
-            Loop Until goA = True
-            Console.WriteLine($"You entered ""{firstNumber}""")
+            Loop
 
 
 
-            Do
-                    Console.WriteLine("Choose a number:")
-                    userResponseB = Console.ReadLine()
-                    Try
-                        secondNumber = CInt(userResponseB)
-                        goB = True
-                    Catch ex As Exception
-                        Select Case userResponseB
-                            Case "q"
-                                quitQue = True
-                                goB = True
-                            Case Else
-                                Console.WriteLine($"You entered ""{userResponseB}"" please enter a whole number.")
-                                goB = False
-                        End Select
+            Do While goB = False
+                Console.WriteLine("Choose a number:")
+                userResponseB = Console.ReadLine()
+                Try
+                    secondNumber = CInt(userResponseB)
+                    goA = True
+                    goB = True
+                    goC = False
+                    goHome = True
+                    Console.WriteLine($"You entered ""{secondNumber}""")
+                Catch ex As Exception
+                    Select Case userResponseB
+                        Case "q"
+                            goA = True
+                            goB = True
+                            goC = True
+                            goHome = True
+                        Case Else
+                            Console.WriteLine($"You entered ""{userResponseB}"" please enter a whole number.")
+                            goB = False
+                    End Select
 
-                    End Try
-                Loop Until goB = True
-
-            Console.WriteLine($"You entered ""{secondNumber}""")
+                End Try
+            Loop
 
 
-        Do
-                    Console.WriteLine(menu)
-                    userResponseC = Console.ReadLine()
-                    Try
-                        thirdNumber = CInt(userResponseC)
-                        Select Case thirdNumber
-                            Case 1
-                                result = firstNumber + secondNumber
-                                Console.WriteLine($"{firstNumber} + {secondNumber} = {result}")
-                                goC = True
-                            Case 2
-                                result = firstNumber - secondNumber
-                                Console.WriteLine($"{firstNumber} - {secondNumber} = {result}")
-                                goC = True
-                            Case 3
-                                result = firstNumber * secondNumber
-                                Console.WriteLine($"{firstNumber} * {secondNumber} = {result}")
-                                goC = True
-                            Case 4
-                                result = firstNumber \ secondNumber
-                                Console.WriteLine($"{firstNumber} \ {secondNumber} = {result}")
-                                goC = True
-                            Case Else
-                                Console.WriteLine($"You entered ""{userResponseC}""")
-                                goC = False
-                        End Select
 
-                    Catch ex As Exception
-                        Console.WriteLine($"You entered ""{userResponseC}""")
-                        goC = False
-                    End Try
+            Do While goC = False
+                Console.WriteLine(menu)
+                userResponseC = Console.ReadLine()
+                Try
+                    thirdNumber = CInt(userResponseC)
+                    Select Case thirdNumber
+                        Case 1
+                            result = firstNumber + secondNumber
+                            Console.WriteLine($"{firstNumber} + {secondNumber} = {result}")
+                            goA = False
+                            goB = False
+                            goC = True
+                            goHome = False
+                        Case 2
+                            result = firstNumber - secondNumber
+                            Console.WriteLine($"{firstNumber} - {secondNumber} = {result}")
+                            goA = False
+                            goB = False
+                            goC = True
+                            goHome = False
+                        Case 3
+                            result = firstNumber * secondNumber
+                            Console.WriteLine($"{firstNumber} * {secondNumber} = {result}")
+                            goA = False
+                            goB = False
+                            goC = True
+                            goHome = False
+                        Case 4
+                            result = firstNumber \ secondNumber
+                            Console.WriteLine($"{firstNumber} \ {secondNumber} = {result}")
+                            goA = False
+                            goB = False
+                            goC = True
+                            goHome = False
+                        Case Else
+                            Console.WriteLine($"You entered ""{userResponseC}""")
+                            goC = False
+                    End Select
 
-                Loop Until goC = True
+                Catch ex As Exception
+                    Select Case userResponseC
+                        Case "q"
+                            goA = True
+                            goB = True
+                            goC = True
+                            goHome = True
+                        Case Else
+                            Console.WriteLine($"You entered ""{userResponseC}""")
+                            goC = False
+                    End Select
+
+                End Try
 
             Loop
 
 
-            Console.WriteLine("Have a nice day")
+        Loop
+
+        Console.WriteLine("Have a nice day")
         Console.ReadLine()
     End Sub
 
